c3a3d3ef636c2d17d82ec970d11b799975732323
thunguyen <nguyenhotoanthuap97@gmail.com>
2021-11-21 10:04:50 +0700

    test csv

--- script.js
+++ script.js
@@ -10,55 +10,27 @@ var svg = d3.select("#onair_datavis")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-$(document).ready(function () {
-  $.ajax({
-    type: "GET",
-    url: "top_100.csv",
-    dataType: "text",
-    success: function (data) {
-      processData(data);
-    },
-  });
-});
-
-function processData(allText) {
-  var allTextLines = allText.split(/\r\n|\n/);
-  var headers = allTextLines[0].split(",");
-  var lines = [];
-
-  for (var i = 1; i < allTextLines.length; i++) {
-    var data = allTextLines[i].split(",");
-    if (data.length == headers.length) {
-      var tarr = [];
-      for (var j = 0; j < headers.length; j++) {
-        tarr.push(headers[j] + ":" + data[j]);
-      }
-      lines.push(tarr);
+d3.csv("https://raw.githubusercontent.com/nguyenhotoanthuap97/datavis-hw3/master/top_100.csv", data => {
+    return {
+      person: data.person,
+      year_month : d3.timeParse("%Y-%m")(data.year_month),
+      screen_time_seconds : data.screen_time_seconds
     }
-  }
-  console.log(lines);
-}
+  }, parsedData => {
 
-d3.csv("top_100.csv", d => {
-
-    console.log(d)
-    parsedData = {
-      person: d.person,
-      year_month : d3.timeParse("%Y-%m")(d.year_month),
-      screen_time_seconds : d.screen_time_seconds
-    }
-
-    console.log(parsedData)
     var stat = d3.nest()
       .key(d => { return d.person; })
       .entries(parsedData);
 
+    console.log (stat)
     var x = d3.scaleTime()
       .domain(d3.extent(parsedData, d => { return d.year_month; }))
       .range([ 0, width ]);
+    var xAxis = d3.axisBottom(x)
+      .tickFormat(d3.timeFormat("%Y-%m"));
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(xAxis);
 
     // Add Y axis
     var y = d3.scaleLinear()
@@ -68,7 +40,7 @@ d3.csv("top_100.csv", d => {
       .call(d3.axisLeft(y));
 
     // Color palette
-    var res = sumstat.map(d => { return d.person })
+    var res = stat.map(d => { return d.person })
     var color = d3.scaleOrdinal()
       .domain(res)
       .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
